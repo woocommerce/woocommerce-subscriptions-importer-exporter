e483900891af7c11c8059b824bffbefc6c36de5f
matttallan <matttallan@gmail.com>
2014-02-02 17:24:58 +1000

    Shop order created and subscriptions being activated. Some of the order item meta data and subscription data needs to be inserted and/or created.

--- include/class-wcsimprt-parser.php
+++ include/class-wcsimprt-parser.php
@@ -67,6 +67,13 @@ class WCS_Import_Parser {
 			"shipping_postcode",
 			"shipping_country",
 		);
+
+		$this->order_item_meta_fields = array (
+			"_recurring_line_total",
+			"_recurring_line_tax",
+			"_recurring_line_subtotal",
+			"_recurring_line_subtotal_tax",
+		);
 	}
 
 	function import_data( $file, $delimiter, $mapping, $start, $end ) {
@@ -170,6 +177,10 @@ class WCS_Import_Parser {
 						$postmeta[] = array( 'key' => '_wcs_requires_manual_renewal', 'value' => 'true' );
 					}
 					break;
+				case 'customer_user':
+
+					$postmeta[] = array( 'key' => '_' . $column, 'value' => $cust_id);
+					break;
 				default:
 					$value = isset( $row[$this->mapping[$column]] ) ? $row[$this->mapping[$column]] : '';
 					if( empty( $value ) ) {
@@ -199,6 +210,52 @@ class WCS_Import_Parser {
 				update_user_meta( $meta['value'], "paying_customer", 1 );
 			}
 		}
+
+		// Add product to the order
+		$_product = get_product( $prod );
+
+		// Add line item
+		$item_id = wc_add_order_item( $order_id, array(
+			'order_item_name' => $_product->get_title(),
+			'order_item_type' => 'line_item'
+		) );
+
+		// Add line item meta
+		if ( $item_id ) {
+			wc_add_order_item_meta( $item_id, '_qty', apply_filters( 'woocommerce_stock_amount', 1 ) );
+			wc_add_order_item_meta( $item_id, '_tax_class', $_product->get_tax_class() );
+			wc_add_order_item_meta( $item_id, '_product_id', $_product->id );
+			wc_add_order_item_meta( $item_id, '_variation_id', '');
+			wc_add_order_item_meta( $item_id, '_line_subtotal', '' );
+			wc_add_order_item_meta( $item_id, '_line_total', '' );
+			wc_add_order_item_meta( $item_id, '_line_tax', '' );
+			wc_add_order_item_meta( $item_id, '_line_subtotal_tax', '' );
+
+			// add the additional subscription meta data to the order
+			foreach( $this->order_item_meta_fields as $metadata ) {
+				$value = ( ! empty( $row[$this->mapping[$metadata]] ) ) ? $row[$this->mapping[$metadata]] : 0;
+				wc_add_order_item_meta( $item_id, $metadata, $value );
+			}
+		}
+
+		// Store variation data in meta so admin can view it
+		/*if ( $values['variation'] && is_array( $values['variation'] ) )
+			foreach ( $values['variation'] as $key => $value )
+			wc_add_order_item_meta( $item_id, esc_attr( str_replace( 'attribute_', '', $key ) ), $value );*/
+
+		// Add line item meta for backorder status
+		if ( $_product->backorders_require_notification() && $_product->is_on_backorder( 1 ) )
+			wc_add_order_item_meta( $item_id, apply_filters( 'woocommerce_backordered_item_meta_name', __( 'Backordered', 'woocommerce' ), $cart_item_key, $order_id ), $values['quantity'] - max( 0, $_product->get_total_stock() ) );
+
+		$subscription_meta[] = array (
+				'start_date'  => ( $row[$this->mapping['start_date']] ) ? $row[$this->mapping['start_date']] : '',
+				'expiry_date' => ( ! empty( $row[$this->mapping['expiry_date']] ) ) ? $row[$this->mapping['expiry_date']] : '',
+			);
+		WC_Subscriptions_Manager::create_pending_subscription_for_order( $order_id, $_product->id, $subscription_meta );
+		
+		$sub_key = WC_Subscriptions_Manager::get_subscription_key( $order_id, $product->id );
+		WC_Subscriptions_Manager::activate_subscription( $cust_id, $sub_key );
+	
 
 		// Attach information on each order to the results array
 		array_push( $this->results, $order_id ); // Test the data correctly adds to the array and is printed to the console
@@ -244,7 +301,10 @@ class WCS_Import_Parser {
 		} else {
 			// check customer id
 			$found_customer = get_user_by( 'id', $customer_id );
-			return $found_customer;
+			if( ! empty( $found_customer ) ) {
+				return absint( $customer_id );
+			}
+			return $found_customer; // should be false
 		}
 	}
 }
