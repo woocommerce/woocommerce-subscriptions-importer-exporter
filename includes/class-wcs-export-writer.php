41e0554b7bcf032307883b485099afcc541e98be
matttallan <matt.d.a@live.com>
2016-01-04 11:33:34 +1000

    Export writer init function which opens the output buffer

--- includes/class-wcs-export-writer.php
+++ includes/class-wcs-export-writer.php
@@ -9,4 +9,18 @@ class WCS_Export_Writer {
 	private static $file = null;
 
 	public static $headers = array();
+
+	/**
+	 * Init function for the export writer. Opens the writer and writes the given CSV headers to the output buffer.
+	 *
+	 * @since 1.0
+	 * @param array $header
+	 */
+	public static function write_headers( $headers ) {
+		self::$file    = fopen( 'php://output', 'w' );
+		ob_start();
+
+		self::$headers = $headers;
+	}
+
 }
