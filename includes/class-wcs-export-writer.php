fa8e97d96c13b94d96c9ed6e12a50df744bf0abc
matttallan <matt.d.a@live.com>
2016-01-04 11:44:29 +1000

    Puts a subscription in csv row array format and writes it to the CSV

--- includes/class-wcs-export-writer.php
+++ includes/class-wcs-export-writer.php
@@ -25,6 +25,134 @@ class WCS_Export_Writer {
 	}
 
 	/**
+	 * Takes the subscription and builds the CSV row based on the headers which have been set, then calls @see self::write().
+	 *
+	 * @since 1.0
+	 * @param WC_Subscription $subscription
+	 */
+	public static function write_subscriptions_csv_row( $subscription ) {
+
+		foreach ( self::$headers as $header_key => $_ ) {
+			switch ( $header_key ) {
+				case 'subscription_id':
+					$value = $subscription->id;
+					break;
+				case 'subscription_status':
+					$value = $subscription->post_status;
+					break;
+				case 'start_date':
+					$value = $subscription->post_date;
+					break;
+				case 'trial_end_date':
+				case 'next_payment_date':
+				case 'end_date':
+					$value = $subscription->get_date( $header_key );
+					break;
+				case 'shipping_total':
+				case 'shipping_tax_total':
+				case 'fee_total':
+				case 'fee_tax_total':
+				case 'tax_total':
+				case 'cart_discount':
+				case 'order_discount':
+				case 'order_total':
+					$value = empty( $subscription->{$header_key} ) ? 0 : $subscription->{$header_key};
+					break;
+				case 'payment_method':
+				case 'shipping_method':
+				case 'billing_first_name':
+				case 'billing_last_name':
+				case 'billing_email':
+				case 'billing_phone':
+				case 'billing_address_1':
+				case 'billing_address_2':
+				case 'billing_postcode':
+				case 'billing_city':
+				case 'billing_state':
+				case 'billing_country':
+				case 'billing_company':
+				case 'shipping_first_name':
+				case 'shipping_last_name':
+				case 'shipping_address_1':
+				case 'shipping_address_2':
+				case 'shipping_postcode':
+				case 'shipping_city':
+				case 'shipping_state':
+				case 'shipping_country':
+				case 'shipping_company':
+				case 'customer_note':
+					$value = $subscription->{$header_key};
+					break;
+				case 'order_notes':
+
+					break;
+				case 'order_items':
+					$value = '';
+
+					foreach ( $subscription->get_items() as $item_id => $item ) {
+
+						$item_meta = new WC_Order_Item_Meta( $item );
+						$meta = $item_meta->display( true, true );
+
+						if ( $meta ) {
+							$meta = str_replace( array( "\r", "\r\n", "\n" ), '', $meta );
+							$meta = str_replace( array( ': ', ':', ';', '|' ), '=', $meta );
+						}
+
+						$line_item = array(
+							'name'     => html_entity_decode( $item['name'], ENT_NOQUOTES, 'UTF-8' ),
+							'quantity' => $item['qty'],
+							'total'    => wc_format_decimal( $subscription->get_line_total( $item ), 2 ),
+							'meta'     => html_entity_decode( $meta, ENT_NOQUOTES, 'UTF-8' ),
+						);
+
+						// add line item tax
+						$line_tax_data    = isset( $item['line_tax_data'] ) ? $item['line_tax_data'] : array();
+						$tax_data         = maybe_unserialize( $line_tax_data );
+						$line_item['tax'] = isset( $tax_data['total'] ) ? wc_format_decimal( wc_round_tax_total( array_sum( (array) $tax_data['total'] ) ), 2 ) : '';
+
+						foreach ( $line_item as $name => $value ) {
+							$line_item[ $name ] = $name . ':' . $value;
+						}
+						$line_item = implode( '|', $line_item );
+
+						if ( $line_item ) {
+							$line_items[] = $line_item;
+							$value = implode( ';', $line_items );
+						}
+					}
+
+					break;
+
+				case 'coupon_items':
+
+					foreach ( $subscription->get_items( 'coupon' ) as $_ => $coupon_item ) {
+
+						$coupon = new WC_Coupon( $coupon_item['name'] );
+
+						$coupon_post = get_post( $coupon->id );
+
+						$coupon_items[] = implode( '|', array(
+							'code:' . $coupon_item['name'],
+							'description:' . ( is_object( $coupon_post ) ? $coupon_post->post_excerpt : '' ),
+							'amount:' . wc_format_decimal( $coupon_item['discount_amount'], 2 ),
+						) );
+					}
+
+					break;
+				case 'download_permissions':
+
+					break;
+				default:
+					$value = '';
+			}
+			$csv_row[ $header_key ] = $value;
+		}
+
+		self::write( $csv_row );
+	}
+
+	/**
 	 * Write line to CSV
 	 *
 	 * @since 1.0
