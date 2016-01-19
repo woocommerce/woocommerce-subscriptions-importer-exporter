6504e54978db424a1425296f34ecc7c352054057
matttallan <matt.d.a@live.com>
2016-01-19 10:24:41 +1000

    Fixes undefined variable usage
    For #99

--- includes/wcsi-functions.php
+++ includes/wcsi-functions.php
@@ -79,7 +79,7 @@ function wcsi_check_customer( $data, $mapped_fields, $test_mode ) {
 				if ( ! is_wp_error( $found_customer ) ) {
 
 					// update user meta data
-					foreach ( self::$user_data_titles as $key ) {
+					foreach ( WCS_Import_Parser::$user_meta_fields as $key ) {
 						switch ( $key ) {
 							case 'billing_email':
 								// user billing email if set in csv otherwise use the user's account email
