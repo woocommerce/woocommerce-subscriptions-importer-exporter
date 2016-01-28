84ca2faa956e361ee1e46a60ad738b0bb7ccc320
matttallan <matt.d.a@live.com>
2016-01-28 14:29:30 +1000

    Fixes fee_items not exporting when only fee_items header is selected

--- includes/class-wcs-export-writer.php
+++ includes/class-wcs-export-writer.php
@@ -34,7 +34,7 @@ class WCS_Export_Writer {
 		$fee_total = $fee_tax_total = 0;
 		$fee_items = array();
 
-		if ( 0 != sizeof( array_intersect( array_keys( self::$headers ), array( 'fee_total', 'fee_tax_total', 'fee_itmes') ) ) ) {
+		if ( 0 != sizeof( array_intersect( array_keys( self::$headers ), array( 'fee_total', 'fee_tax_total', 'fee_items') ) ) ) {
 			foreach ( $subscription->get_fees() as $fee_id => $fee ) {
 
 				$fee_items[] = implode( '|', array(
