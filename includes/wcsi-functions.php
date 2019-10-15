1c69a8b258b4a88223f0097795d1f518df309053
Shamali Sulakhe <shamali.sulakhe@wisdmlabs.com>
2019-10-15 20:07:11 +0530

    Updated comment.

--- includes/wcsi-functions.php
+++ includes/wcsi-functions.php
@@ -8,7 +8,7 @@
  * @param string $file_encoding
  */
 function wcsi_format_data( $data, $file_encoding = 'UTF-8' ) {
-	// Check if the function utf8_encode( $data ) exists. The function does not found if the php-xml extension is not installed on the server.
+	// Check if the function utf8_encode exists. The function does not found if the php-xml extension is not installed on the server.
 	$return = function_exists('utf8_encode') ? utf8_encode( $data ) : $data;
 	return ( 'UTF-8' == $file_encoding ) ? $data : $return;
 }
