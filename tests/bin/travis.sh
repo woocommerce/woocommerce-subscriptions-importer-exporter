28b26b57f907662be00d247bb12f0c9bba4083f3
Omkar Bhagat <codestor4@gmail.com>
2020-10-21 18:01:27 +0530

    Update subscriptins github repo URL
    Updating it from
    Prospress/woocommerce-subscriptions
    to
    woocommerce/woocommerce-subscriptions

--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -16,7 +16,7 @@ if [ $1 == 'before' ]; then
 	say "WooCommerce Installed"
 
 	# place a copy of woocommerce subscriptions where the unit tests etc. expect it to be - needs to be a repo with the test dir
-	git clone https://$GITHUB_TOKEN@github.com/Prospress/woocommerce-subscriptions.git "../woocommerce-subscriptions" -b $WCS_VERSION
+	git clone https://$GITHUB_TOKEN@github.com/woocommerce/woocommerce-subscriptions.git "../woocommerce-subscriptions" -b $WCS_VERSION
 
 	say "WooCommerce Subscriptions Installed"
 
