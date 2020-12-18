0adf26231cb7276ec6012110029d50e3e6b0c886
Achyuth Ajoy <achyuthajoy@gmail.com>
2020-12-18 16:49:30 +0530

    Script Refactoring

--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -8,14 +8,12 @@ say() {
 }
 
 if [ $1 == 'before' ]; then
-
 	# place a copy of woocommerce where the unit tests etc. expect it to be
 	mkdir -p "../woocommerce"
 	curl -L https://api.github.com/repos/woothemes/woocommerce/tarball/$WC_VERSION?access_token=$GITHUB_TOKEN --silent | tar --strip-components=1 -zx -C "../woocommerce"
-
 	say "WooCommerce Installed"
 
+	# Install WooCommerce Subscriptions.
+	git clone https://$GITHUB_TOKEN@github.com/woocommerce/woocommerce-subscriptions.git "../woocommerce-subscriptions" --branch $WCS_VERSION
+	say "WooCommerce Subscriptions Installed"
 fi
-
-# Install WooCommerce Subscriptions.
-git clone https://$GITHUB_TOKEN@github.com/woocommerce/woocommerce-subscriptions.git "../woocommerce-subscriptions" --branch $WCS_VERSION
